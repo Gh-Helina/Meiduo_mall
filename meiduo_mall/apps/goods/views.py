ed6503eb1d407030ffc4c2f3c924aaf2f3f94a7a
helina <hln@163.com>
2019-10-24 17:36:03 +0800

    商品排序

--- meiduo_mall/apps/goods/views.py
+++ meiduo_mall/apps/goods/views.py
@@ -18,4 +18,20 @@ class ListView(View):
             return render(request, '404.html')
         # 1.根据分类，查询所有数据
         data=SKU.objects.filter(category=category)
+
+        # 2.添加排序
+        # default:上架时间
+        # hot:热销
+        # price:价格
+        sort=request.GET.get('sort')
+        if sort=='defalt':
+            order_filed='create_time'
+        elif sort=='hot':
+            order_filed='sales'
+        else:
+            order_filed='-price'
+        data=SKU.objects.filter(category=category).order_by(order_filed)
+        # 3.添加分页
+
         return render(request, 'list.html')
+        # return
