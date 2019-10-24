a4183016ec20e727d374861f15f1ef80d81065e4
helina <hln@163.com>
2019-10-24 20:24:09 +0800

    详情页

--- meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/apps/goods/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page>\d+)/$',views.ListView.as_view(),name='list'),
     #热销页
     url(r'^hot/(?P<category_id>\d+)/$',views.HotGoodsView.as_view(),name='hot'),
+    #详情页
+    url(r'^detail/(?P<sku_id>\d+)/$',views.DetailView.as_view(),name='detail'),
 ]
