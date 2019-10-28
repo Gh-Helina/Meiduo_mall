d9fc73f29b39f6cdee030990d7c6925b795cf70c
helina <hln@163.com>
2019-10-28 20:48:37 +0800

    RabbitMQ

--- meiduo_mall/celery_tasks/config.py
+++ meiduo_mall/celery_tasks/config.py
@@ -1,4 +1,5 @@
-# 中间人链接14号库
-broker_url='redis://127.0.0.1/14'
-# 结果存储到15号库
-result_backend='redis://127.0.0.1/15'
+# # 中间人链接14号库
+# broker_url='redis://127.0.0.1/14'
+# # 结果存储到15号库
+# result_backend='redis://127.0.0.1/15'
+broker_url= 'amqp://guest:guest@127.0.0.1:5672'
